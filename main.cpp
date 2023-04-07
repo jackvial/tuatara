d05f1f00d5632fa2aef4cba56f7de85149b1e2a4
Jack Vial <vialjack@gmail.com>
2023-04-06 22:38:37 -0400

    Rendered heatmap but it looks squashed

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+using namespace torch::indexing;
+
 // int parseq()
 // {
 //     std::string model_path = "/Users/jackvial/Code/CPlusPlus/torchscript_example/parseq-tiny/torchscript_model.bin";
@@ -51,6 +53,22 @@
 
 //     return 0;
 // }
+
+void print_tensor_dims(std::string label, torch::Tensor t)
+{
+    int64_t num_dims = t.dim();
+    // Print the dimensions of the tensor
+    std::cout << label << " (";
+    for (int64_t i = 0; i < num_dims; ++i)
+    {
+        std::cout << t.size(i);
+        if (i < num_dims - 1)
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << ")" << std::endl;
+}
 
 int main(int argc, const char *argv[])
 {
@@ -113,7 +131,40 @@ int main(int argc, const char *argv[])
         torch::Tensor output_tensor_1 = output_tuple->elements()[0].toTensor();
         torch::Tensor output_tensor_2 = output_tuple->elements()[1].toTensor();
 
-        std::cout << "Done!";
+        print_tensor_dims(" output_tensor_1 ", output_tensor_1);
+
+        // Get the size of the batch dimension
+        int64_t batch_size = output_tensor_1.size(0);
+
+        // Iterate through the batch dimension
+        for (int64_t batch_index = 0; batch_index < batch_size; ++batch_index)
+        {
+            // Access the tensor corresponding to the current batch
+            torch::Tensor current_batch = output_tensor_1[batch_index];
+            print_tensor_dims(" current_batch ", current_batch);
+
+            // Separate the tensor into two tensors of dimensions 624 x 336
+            torch::Tensor score_text = current_batch.slice(2, 0, 1).squeeze(2);
+            torch::Tensor score_link = current_batch.slice(2, 1, 2).squeeze(2);
+
+            print_tensor_dims(" score_text ", score_text);
+            print_tensor_dims(" score_link ", score_link);
+
+            // Normalize the tensor to the range [0, 1]
+            torch::Tensor tensor_normalized = (score_text - score_text.min()) / (score_text.max() - score_text.min());
+
+            // Convert the normalized tensor to an OpenCV Mat
+            cv::Mat mat(tensor_normalized.size(0), tensor_normalized.size(1), CV_32F, tensor_normalized.data_ptr<float>());
+
+            // Convert the normalized Mat to a heatmap
+            cv::Mat heatmap;
+            mat.convertTo(heatmap, CV_8UC1, 255);
+            cv::applyColorMap(heatmap, heatmap, cv::COLORMAP_JET);
+
+            // Display the heatmap
+            cv::imshow("Heatmap", heatmap);
+            cv::waitKey(0);
+        }
 
         // std::cout << "Output tensor 1: " << output_tensor_1 << "\n";
         // std::cout << "Output tensor 2: " << output_tensor_2 << "\n";
