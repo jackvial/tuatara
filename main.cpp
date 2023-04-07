1dd8646b20496184b7c92d40b504328ef2e1c4b7
Jack Vial <vialjack@gmail.com>
2023-04-07 17:53:31 -0400

    Added box detection

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <memory>
+#include <vector>
+#include <cmath>
+#include <utility>
 #include <torch/script.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -88,6 +91,121 @@ void display_2d_tensor_heatmap(torch::Tensor t)
     cv::waitKey(0);
 }
 
+std::pair<std::vector<cv::RotatedRect>, cv::Mat> get_detected_boxes(
+    torch::Tensor textmap, torch::Tensor linkmap, float text_threshold,
+    float link_threshold, float low_text, bool estimate_num_chars = false)
+{
+    // Convert the normalized tensor to an OpenCV Mat
+    cv::Mat textmap_cv(textmap.size(0), textmap.size(1), CV_32F, textmap.data_ptr<float>());
+    // cv::Mat textmap_cv = textmap.to(torch::kF32).mul(255).clamp(0, 255).to(torch::kU8).squeeze().detach().numpy().clone();
+
+    cv::Mat linkmap_cv(linkmap.size(0), linkmap.size(1), CV_32F, linkmap.data_ptr<float>());
+    // cv::Mat linkmap_cv = linkmap.to(torch::kF32).mul(255).clamp(0, 255).to(torch::kU8).squeeze().detach().numpy().clone();
+
+    int img_h = textmap_cv.rows;
+    int img_w = textmap_cv.cols;
+
+    cv::Mat text_score, link_score;
+    cv::threshold(textmap_cv, text_score, low_text, 1, 0);
+    cv::threshold(linkmap_cv, link_score, link_threshold, 1, 0);
+
+    cv::Mat text_score_comb = cv::min(text_score + link_score, 1);
+    text_score_comb.convertTo(text_score_comb, CV_8U);
+
+    cv::Mat labels, stats;
+    cv::Mat centroids;
+    int nLabels = cv::connectedComponentsWithStats(text_score_comb, labels, stats, centroids);
+
+    std::vector<cv::RotatedRect> det;
+    std::vector<int> mapper;
+    for (int k = 1; k < nLabels; ++k)
+    {
+        int size = stats.at<int>(k, cv::CC_STAT_AREA);
+        if (size < 10)
+            continue;
+
+        cv::Mat mask = (labels == k);
+        double minVal, maxVal;
+        cv::minMaxLoc(textmap_cv, &minVal, &maxVal, nullptr, nullptr, mask);
+
+        if (maxVal < text_threshold)
+            continue;
+
+        cv::Mat segmap = cv::Mat::zeros(textmap_cv.size(), CV_8UC1);
+        segmap.setTo(255, labels == k);
+
+        if (estimate_num_chars)
+        {
+            cv::Mat character_locs;
+            cv::threshold(((textmap_cv - linkmap_cv).mul(segmap) / 255.0), character_locs, text_threshold, 1, 0);
+            cv::Mat labels_characters;
+            int n_chars = cv::connectedComponents(character_locs, labels_characters);
+            mapper.push_back(n_chars);
+        }
+        else
+        {
+            mapper.push_back(k);
+        }
+
+        segmap.setTo(0, (link_score == 1) & (text_score == 0));
+
+        int x = stats.at<int>(k, cv::CC_STAT_LEFT);
+        int y = stats.at<int>(k, cv::CC_STAT_TOP);
+        int w = stats.at<int>(k, cv::CC_STAT_WIDTH);
+        int h = stats.at<int>(k, cv::CC_STAT_HEIGHT);
+        int niter = static_cast<int>(std::sqrt(size * std::min(w, h) / (w * h) * 2));
+
+        int sx = std::max(0, x - niter);
+        int sy = std::max(0, y - niter);
+        int ex = std::min(img_w, x + w + niter + 1);
+        int ey = std::min(img_h, y + h + niter + 1);
+
+        cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(1 + niter, 1 + niter));
+        cv::dilate(segmap(cv::Rect(sx, sy, ex - sx, ey - sy)), segmap(cv::Rect(sx, sy, ex - sx, ey - sy)), kernel);
+
+        std::vector<cv::Point> np_contours;
+        cv::findNonZero(segmap, np_contours);
+        cv::RotatedRect rectangle = cv::minAreaRect(np_contours);
+        cv::Point2f box[4];
+        rectangle.points(box);
+
+        float box_w = cv::norm(box[0] - box[1]);
+        float box_h = cv::norm(box[1] - box[2]);
+        float box_ratio = std::max(box_w, box_h) / (std::min(box_w, box_h) + 1e-5);
+
+        if (std::abs(1 - box_ratio) <= 0.1)
+        {
+            int l = std::min_element(np_contours.begin(), np_contours.end(),
+                                     [](const cv::Point &a, const cv::Point &b)
+                                     { return a.x < b.x; })
+                        ->x;
+            int r = std::max_element(np_contours.begin(), np_contours.end(),
+                                     [](const cv::Point &a, const cv::Point &b)
+                                     { return a.x < b.x; })
+                        ->x;
+            int t = std::min_element(np_contours.begin(), np_contours.end(),
+                                     [](const cv::Point &a, const cv::Point &b)
+                                     { return a.y < b.y; })
+                        ->y;
+            int b = std::max_element(np_contours.begin(), np_contours.end(),
+                                     [](const cv::Point &a, const cv::Point &b)
+                                     { return a.y < b.y; })
+                        ->y;
+
+            cv::Point2f new_box[4] = {cv::Point2f(l, t), cv::Point2f(r, t), cv::Point2f(r, b), cv::Point2f(l, b)};
+            std::copy(new_box, new_box + 4, box);
+        }
+
+        int startidx = std::distance(box, std::min_element(box, box + 4, [](const cv::Point2f &a, const cv::Point2f &b)
+                                                           { return a.x + a.y < b.x + b.y; }));
+        std::rotate(box, box + startidx, box + 4);
+
+        det.emplace_back(rectangle);
+    }
+
+    return std::make_pair(det, labels);
+}
+
 int main(int argc, const char *argv[])
 {
     // if (argc != 2)
@@ -121,7 +239,7 @@ int main(int argc, const char *argv[])
     }
 
     // torch.Size([1, 3, 672, 1248])
-    cv::resize(image, image, cv::Size(1248, 672));
+    cv::resize(image, image, cv::Size(312 * 4, 168 * 4));
     cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
 
     torch::Tensor image_tensor = torch::from_blob(
@@ -166,8 +284,47 @@ int main(int argc, const char *argv[])
             print_tensor_dims(" score_text ", score_text);
             print_tensor_dims(" score_link ", score_link);
 
-            display_2d_tensor_heatmap(score_text);
-            display_2d_tensor_heatmap(score_link);
+            // display_2d_tensor_heatmap(score_text);
+            // display_2d_tensor_heatmap(score_link);
+
+            // Set parameters for the function
+            float text_threshold = 0.7;
+            float link_threshold = 0.4;
+            float low_text = 0.1;
+            bool estimate_num_chars = false;
+            auto result = get_detected_boxes(score_text, score_link, text_threshold, link_threshold, low_text, estimate_num_chars);
+
+            // Extract results
+            auto det = result.first;
+            auto labels = result.second;
+
+            // Print results
+            std::cout << "Detected boxes: " << det.size() << std::endl;
+            for (const auto &box : det)
+            {
+                cv::Point2f corners[4];
+                box.points(corners);
+                std::cout << "Box: ";
+                for (int i = 0; i < 4; ++i)
+                {
+                    std::cout << "(" << corners[i].x << ", " << corners[i].y << ") ";
+                }
+                std::cout << std::endl;
+            }
+
+            // Draw the detected boxes on the image
+            for (const auto &box : det)
+            {
+                cv::Point2f corners[4];
+                box.points(corners);
+                std::vector<cv::Point> corners_vec(corners, corners + 4);
+                cv::polylines(image, corners_vec, true, cv::Scalar(0, 255, 0), 2);
+            }
+
+            // Display the image with the drawn boxes
+            cv::namedWindow("Detected Boxes", cv::WINDOW_NORMAL);
+            cv::imshow("Detected Boxes", image);
+            cv::waitKey(0);
         }
 
         // std::cout << "Output tensor 1: " << output_tensor_1 << "\n";
