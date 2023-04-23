c96d0a911889697397245484b622f25d139ea86a
Jack Vial <vialjack@gmail.com>
2023-04-23 17:01:35 -0400

    Formatted examples

--- examples/resume.cpp
+++ examples/resume.cpp
@@ -1,17 +1,13 @@
-#include "tuatara.h"
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 
+#include "tuatara.h"
+
 // Main file
 int main(int argc, const char** argv) {
-     std::string image_path = argv[1];
-     cv::Mat image = cv::imread(image_path, cv::IMREAD_COLOR);
-    
-    image_to_data(
-        image,
-        argv[2],
-        argv[3],
-        argv[4]
-    );
-    return 0;
+  std::string image_path = argv[1];
+  cv::Mat image = cv::imread(image_path, cv::IMREAD_COLOR);
+
+  image_to_data(image, argv[2], argv[3], argv[4]);
+  return 0;
 }
--- examples/table.cpp
+++ examples/table.cpp
@@ -1,16 +1,12 @@
-#include "tuatara.h"
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 
+#include "tuatara.h"
+
 // Main file
 int main(int argc, const char** argv) {
-    std::string image_path = argv[1];
-    cv::Mat image = cv::imread(image_path, cv::IMREAD_COLOR);
-    image_to_data(
-        image,
-        "../../weights",
-        "../../outputs",
-        "0"
-    );
-    return 0;
+  std::string image_path = argv[1];
+  cv::Mat image = cv::imread(image_path, cv::IMREAD_COLOR);
+  image_to_data(image, "../../weights", "../../outputs", "0");
+  return 0;
 }
